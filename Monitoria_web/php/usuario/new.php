6201b38aae564168ef6dbf70033007f858320048
juliotoscano <juliotoscanodebrito@gmail.com>
2017-09-19 22:40:47 -0300

    Utilizando Bootstrap 4.0 aphl

--- Monitoria_web/php/usuario/new.php
+++ Monitoria_web/php/usuario/new.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	include '../conexao_session.php';
  ?>
 <!DOCTYPE html>
@@ -7,12 +7,27 @@
 	<title>New user</title>
 </head>
 <body>
-	<h4>Cadastro</h4>
-	<form action="new_conect.php" method="POST" class="formulario">
-		<input type="text" name="nome" placeholder="Digite seu Nome ex:Joao Jose">
-		<input type="password" name="senha" placeholder="Digite sua Senha ex: Joao123">
-		<input type="submit" name="enviar" value="Cadastrar">
-	</form>
-	
+        <center>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-head">
+          	           <h4>Cadastro</h4>
+                </div>
+                <div class="card-body">
+                    <form action="new_conect.php" method="POST" >
+                        <div class="form-group">
+                    	<input type="text" class="form-control" name="nome" aria-describedby="NomeHelp" placeholder="Digite seu Nome ex:Joao Jose">
+                                <small id="NomeHelp" class="form-text text-muted">Nome para ser identificado no Sistema</small>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="senha" placeholder="Digite sua Senha ex: Joao123">
+                        </div>
+                            <input type="submit" name="enviar" value="Cadastrar">
+        	   </form>
+                </div>
+            </div>
+        </div>
+        </center>
+
 </body>
 </html>
